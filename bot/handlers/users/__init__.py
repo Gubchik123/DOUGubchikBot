3b1a78ff6abe4005af9bc4703f6e08aab4cf1bad
Nikita <nikita.gubarev31@gmail.com>
2024-08-04 12:20:49 +0300

    Update the handlers/ __init__.py files and remove non-existent modules

--- bot/handlers/users/__init__.py
+++ bot/handlers/users/__init__.py
@@ -2,19 +2,11 @@ from aiogram import Router
 
 from .commands import commands_router
 from .menu import router as menu_router
-from .profile import profile_router
-from .weather import weather_router
-from .mailing import mailing_router
 from .other import router as other_router
 
 
 users_router = Router()
 
 users_router.include_routers(  # ! Order is important
-    commands_router,
-    menu_router,
-    profile_router,
-    mailing_router,
-    weather_router,
-    other_router,
+    commands_router, menu_router, other_router
 )
