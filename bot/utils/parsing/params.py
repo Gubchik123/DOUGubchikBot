12594e5eafc64f42149b46d35be2419897173e9b
Nikita <nikita.gubarev31@gmail.com>
2024-08-05 16:34:53 +0300

    Create utils/parsing/params.py with Params(NamedTuple)

---
+++ bot/utils/parsing/params.py
@@ -0,0 +1,25 @@
+from typing import NamedTuple, Optional
+
+
+class Params(NamedTuple):
+    """Named tuple to store URL params that necessary for searching."""
+
+    category: str
+    exp: str
+    city: Optional[str] = None
+    remote: Optional[bool] = False
+    relocate: Optional[bool] = False
+    search: Optional[str] = None
+    descr: Optional[bool] = False  # search in jobs descriptions
+
+    def __str__(self) -> str:
+        """Returns string representation: URL params."""
+        if self.remote:
+            prefix = "?remote&"
+        elif self.relocate:
+            prefix = "?relocate&"
+        else:
+            prefix = "?"
+        return prefix + "&".join(
+            f"{key}={value}" for key, value in self._asdict().items() if value
+        )
