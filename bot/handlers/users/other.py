87737a23110a70edfd35c849cdc54a6ba1dab6a5
Nikita <nikita.gubarev31@gmail.com>
2024-08-06 15:12:39 +0300

    Translate the error (other) message to Ukrainian

--- bot/handlers/users/other.py
+++ bot/handlers/users/other.py
@@ -13,8 +13,8 @@ async def handle_all_other_messages(message: Message):
     try:
         await message.answer(
             _(
-                "I don't understand you :(\n"
-                "I advise you to use the buttons or commands for the intended result."
+                "Я Вас не розумію :(\n"
+                "Раджу використати кнопки або команди для задуманого результату."
             )
         )
     except TelegramBadRequest:
