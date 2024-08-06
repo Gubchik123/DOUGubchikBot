7849bfdb293308166669b513d2367ddb18c64655
Nikita <nikita.gubarev31@gmail.com>
2024-08-06 14:54:24 +0300

    Add the check vacancies button handler to vacancy menu

--- bot/handlers/users/vacancy/menu.py
+++ bot/handlers/users/vacancy/menu.py
@@ -1,8 +1,11 @@
+from random import choice
+
 from aiogram import Router, F
 from aiogram.types import Message
-from aiogram.utils.i18n import lazy_gettext as __
+from aiogram.utils.i18n import gettext as _, lazy_gettext as __
 
 from utils.decorators import vacancy_required
+from utils.scheduler import check_vacancies_by_
 from utils.db.models import Vacancy
 from utils.db.crud.vacancy import update_vacancy_with_
 from messages.vacancy.menu import get_vacancy_menu_message_by_
@@ -34,3 +37,22 @@ async def handle_not_search_job(message: Message, vacancy: Vacancy, *args):
     """Handles the not search job button."""
     update_vacancy_with_(user_chat_id=vacancy.id_user_id, active=False)
     await handle_vacancy_menu(message, vacancy)
+
+
+@router.message(F.text.lower() == __("перевірити вакансії"))
+@vacancy_required
+async def handle_check_vacancies(message: Message, vacancy: Vacancy, *args):
+    """Handles the check vacancies button."""
+    was_vacancies = await check_vacancies_by_(vacancy)
+    if was_vacancies is False:
+        await message.answer_sticker(
+            choice(
+                [
+                    "CAACAgIAAxkBAAEMmw9msNROM8A5ywuyDTjoobwhmEvqCAACEg0AAtqrwUpUjHmjHVNcEjUE",
+                    "CAACAgIAAxkBAAEMm9JmsguptxkU2xwf3g7mW-JKvuR67gACPQ0AAu7hYEki8vUJecaK4DUE",
+                    "CAACAgIAAxkBAAEMm9Rmsgut4PnLYYm0JJctuVxqfVr8OwACoQ4AAgkouErdJlk-PMSKuDUE",
+                    "CAACAgIAAxkBAAEMm9BmsgtD1ihaAAFK_SexzLlE3XeXnZIAAlUMAAJTN1lJ7r4gyMng9Eg1BA",
+                ]
+            )
+        )
+        await message.answer(_("Нових вакансій не знайдено!"))
