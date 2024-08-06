f417473353c24a5f7e4ce6aa1645a31c556e5ea9
Nikita <nikita.gubarev31@gmail.com>
2024-08-06 14:04:11 +0300

    Extract the vacancy menu message into new function in new messages/vacancy/menu.py

--- bot/handlers/users/vacancy/menu.py
+++ bot/handlers/users/vacancy/menu.py
@@ -1,8 +1,8 @@
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.utils.i18n import gettext as _
 
 from utils.db.models import Vacancy
+from messages.vacancy.menu import get_vacancy_menu_message_by_
 
 
 router = Router()
@@ -10,16 +10,4 @@ router = Router()
 
 async def handle_vacancy_menu(message: Message, vacancy: Vacancy):
     """Handles vacancy menu."""
-    await message.answer(
-        _(
-            "<b>Меню пошуку</b>\n\n"
-            "Шукаєте роботу зараз? {vacancy.active}\n\n"
-            "Категорія: {vacancy.category}\n"
-            "Досвід: {vacancy.exp}\n"
-            "Місто: {vacancy.city}\n\n"
-            "Віддалена робота? {vacancy.remote}\n"
-            "За кордоном? {vacancy.relocate}\n"
-            "Пошуковий запит: {vacancy.search}\n"
-            "Шукати в описі вакансій? {vacancy.descr}\n"
-        ).format(vacancy=vacancy)
-    )
+    await message.answer(get_vacancy_menu_message_by_(vacancy))
---
+++ bot/messages/vacancy/menu.py
@@ -0,0 +1,28 @@
+from aiogram.utils.i18n import gettext as _
+
+from utils.db.models import Vacancy
+
+
+def get_vacancy_menu_message_by_(vacancy: Vacancy) -> str:
+    """Returns formatted vacancy menu message by the given vacancy."""
+    city = vacancy.city
+    if vacancy.remote:
+        city = _("віддалена робота")
+    elif vacancy.relocate:
+        city = _("за кордоном")
+    return _(
+        "<b>Меню пошуку</b>\n\n{active}\n\n"
+        "Категорія: {category}\nДосвід: {exp}\nМісто: {city}\n\n"
+        "Пошуковий запит: {search} {descr}\n"
+    ).format(
+        active=(
+            _("Шукаєте роботу зараз")
+            if vacancy.active
+            else _("Не шукаєте роботу зараз")
+        ),
+        category=vacancy.category,
+        exp=_("Без досвіду") if not vacancy.exp else vacancy.exp,
+        city=city,
+        search="-" if not vacancy.search else vacancy.search,
+        descr=_("(шукати в описі вакансій)") if vacancy.descr else "",
+    )
