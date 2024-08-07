1677c97ac80c99b0e0c3c1f1ce454e5c704805ec
Nikita <nikita.gubarev31@gmail.com>
2024-08-07 12:51:40 +0300

    Update the scheduler interval to 3 hours

--- bot/run.py
+++ bot/run.py
@@ -77,7 +77,7 @@ def _start_scheduler() -> None:
     """Starts the scheduler and adds interval job."""
     scheduler.start()
     scheduler.remove_all_jobs()
-    scheduler.scheduled_job(trigger="interval", hours=1)(
+    scheduler.scheduled_job(trigger="interval", hours=3)(
         check_vacancies_for_all_users
     )
     scheduler.print_jobs()
